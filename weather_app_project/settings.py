4d1b80aa88e6fd7845262a998c085a3f19fb1ca7
Akshay <akshay@tyloones.com>
2024-05-09 14:40:50 +0530

    .

--- weather_app_project/settings.py
+++ weather_app_project/settings.py
@@ -60,9 +60,7 @@ ROOT_URLCONF = 'weather_app_project.urls'
 
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:5173', 
-    'http://13.234.43.50:7000/',
-    'http://13.234.43.50:7001/',
-    'http://13.234.43.50:7002/', 
+     'http://13.233.31.248:5000/',  
 ]
 CORS_ALLOW_HEADERS = ['Content-Type', 'Email', 'City', 'Authorization'] 
 
